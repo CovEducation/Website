7e0c3fb3df62aeaa652cd61f860c7c6bfeb37735
Sanjay Yepuri <sanjayyepuri@users.noreply.github.com>
2021-03-25 10:02:20 -0500

    added the ability to add, remove, and modify students (#111)
    * add people to team
    
    * Added sanjay and grace
    
    * removed search filters for now
    
    * Added an Error Message when User Data Fails to Load (#94)
    
    * added error message instead of plain Loading
    
    * fix log in bug
    
    * add resources, fix h2 nit, accordion nit (#98)
    
    * Fixed typo
    
    * Yank code
    
    * added the ability to add, remove, and modify students
    
    * Added student edit support on the backend
    
    * Added student edit support on the backend
    
    Co-authored-by: Dheekshita Kumar <dhkumar@mit.edu>
    Co-authored-by: Johan Cervantes <45345485+johancc@users.noreply.github.com>
    Co-authored-by: Johan Cervantes <jccamacho19992@gmail.com>
    Co-authored-by: DheekshuKumar <DheekshuKumar@users.noreply.github.com>

--- packages/client/src/pages/Profile/sections.js
+++ packages/client/src/pages/Profile/sections.js
@@ -7,7 +7,19 @@ import {
   Divider,
   Button,
   ButtonGroup,
+  IconButton,
+  Menu,
+  MenuItem,
+  ListItemAvatar,
+  ListItemText,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
 } from "@material-ui/core";
+
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 import {
   Timezones,
@@ -28,6 +40,8 @@ import {
   FormikSelect,
   FormikRadio,
 } from "../../components/SignUp2/fields";
+
+import ProfilePicture from "../../components/ProfilePicture";
 
 import { useFormik } from "formik";
 import * as Yup from "yup";
@@ -83,6 +97,7 @@ const ProfileRow = ({
       return (
         <FormikRadio
           name={name}
+          value={value}
           label={label}
           values={values}
           formik={formik}
@@ -134,10 +149,7 @@ const submitSection = (onSubmit) => {
   // This validation should be moved somewhere else.
 
   return (values) => {
-    if (values.available) {
-      values.available = values.available === "YES";
-    }
-    onSubmit(values);
+    onSubmit({ ...values, available: values.available === "YES" });
   };
 };
 
@@ -247,7 +259,8 @@ export const UserDetails = ({ values, updateFields }) => {
 const mapJoin = (list, map) => list.map((e) => map[e]).join(", ");
 
 export const MentorDetails = ({ values, updateFields }) => {
-  values.available = values.available ? "YES" : "NO";
+  values.available =
+    values.available === true || values.available === "YES" ? "YES" : "NO";
   // this is duplicated code, think about moving...
   const MentorSchema = Yup.object({
     college: Yup.string(),
@@ -255,7 +268,7 @@ export const MentorDetails = ({ values, updateFields }) => {
     introduction: Yup.string().required("Bio required"),
     major: Yup.string(),
     subjects: Yup.array().required("Subjects Required"),
-    available: Yup.string(),
+    available: Yup.string().default(values.available),
   });
 
   const onCancel = () => {
@@ -346,7 +359,7 @@ export const MentorDetails = ({ values, updateFields }) => {
         <ProfileRow
           name="available"
           label="Available to Mentor"
-          value={availabilitiesVM[available] || "No"}
+          value={availabilitiesVM[available]}
           edit={edit}
           type={Fields.RADIO}
           values={availabilities}
@@ -358,21 +371,220 @@ export const MentorDetails = ({ values, updateFields }) => {
   );
 };
 
-const StudentDetail = ({ student }) => {
-  const subjects = mapJoin(student.subjects, SubjectsVM);
+const StudentMenu = ({ onEdit, onRemove }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleEdit = () => {
+    onEdit();
+    handleClose();
+  };
+
+  const handleRemove = () => {
+    onRemove();
+    handleClose();
+  };
+
   return (
-    <ListItem key={student.name}>
-      <Grid container spacing={SPACING}>
-        <ProfileRow label="Name" value={student.name} />
-        <ProfileRow label="Grade level" value={student.gradeLevel} />
-        <ProfileRow label="Subjects" value={subjects} />
-      </Grid>
-      <Divider />
-    </ListItem>
+    <div>
+      <IconButton
+        aria-controls="more"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="more"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem key="edit" onClick={handleEdit}>Edit</MenuItem>
+        <MenuItem key="delete" onClick={handleRemove}>Delete</MenuItem>
+      </Menu>
+    </div>
   );
 };
 
-export const ParentStudentDetails = ({ values }) => {
+const RemoveStudentConfirmation = ({
+  open,
+  handleDelete,
+  handleClose,
+  name,
+}) => {
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {" "}
+          Are you sure you want to delete {` ${name}`} permanently?
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            This action cannot be undone. Any mentorships with this student will
+            be deleted.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleDelete} color="secondary" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+const StudentDetail = ({
+  student,
+  updateStudent,
+  removeStudent,
+  onComplete,
+  isNew,
+}) => {
+  const StudentSchema = Yup.object({
+    name: Yup.string().required("Name required."),
+    subjects: Yup.array().required(),
+    gradeLevel: Yup.string().required(),
+  });
+
+  const [formEdit, setEdit] = React.useState(false);
+  const [removeDialogOpen, setRemoveDialogOpen] = React.useState(false);
+
+  const onCancel = () => {
+    setEdit(false);
+    if (onComplete !== undefined) onComplete();
+    formik.resetForm({ value: student });
+  };
+
+  const onSubmit = (values) => {
+    updateStudent(values)
+      .then(() => setEdit(false))
+      .then(() => onComplete && onComplete());
+  };
+
+  const handleDelete = () => {
+    removeStudent()
+      .then(() => setRemoveDialogOpen(false));
+  }
+
+  const formik = useFormik({
+    initialValues: student,
+    validationSchema: StudentSchema,
+    onSubmit: onSubmit,
+  });
+
+  const edit = formEdit || isNew;
+
+  const { name, gradeLevel, subjects } = formik.values;
+
+  const subjectsJoined = mapJoin(subjects, SubjectsVM);
+
+  return (
+    <div>
+      <ListItem key={student.name}>
+        <ListItemAvatar>
+          <ProfilePicture size={86} value={student.name} />
+        </ListItemAvatar>
+        <ListItemText>
+          <Grid container spacing={SPACING}>
+            <Grid item md={8}>
+              <form onSubmit={formik.handleSubmit}>
+                <Grid container spacing={SPACING}>
+                  <Grid item sm={12}>
+                    {" "}
+                    <h4>{student.name}</h4>{" "}
+                  </Grid>
+                  <ProfileRow
+                    name="name"
+                    label="Name"
+                    value={name}
+                    edit={edit}
+                    type={Fields.TEXT}
+                    formik={formik}
+                  />
+                  <ProfileRow
+                    name="gradeLevel"
+                    label="Grade Level"
+                    value={gradeLevel}
+                    edit={edit}
+                    type={Fields.SELECT}
+                    values={GradeLevels}
+                    formik={formik}
+                  />
+                  <ProfileRow
+                    name="subjects"
+                    label="Subjects"
+                    value={subjectsJoined}
+                    edit={edit}
+                    type={Fields.SELECT}
+                    values={Subjects}
+                    isMulti
+                    formik={formik}
+                  />
+                  {edit && <SubmitButton onCancel={onCancel} />}
+                </Grid>
+              </form>
+            </Grid>
+            <Grid item md={4}>
+              <StudentMenu
+                onEdit={() => setEdit(true)}
+                onRemove={() => setRemoveDialogOpen(true)}
+              />
+            </Grid>
+          </Grid>
+        </ListItemText>
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <RemoveStudentConfirmation
+        name={name}
+        open={removeDialogOpen}
+        handleClose={() => setRemoveDialogOpen(false)}
+        handleDelete={handleDelete}
+      />
+    </div>
+  );
+};
+
+export const ParentStudentDetails = ({ values, updateFields }) => {
+  const [addingStudent, setAddingStudent] = React.useState(false);
+  const [students, setStudents] = React.useState(values.students);
+
+  const addStudent = (student) => {
+    values.students.push(student);
+    return updateFields(values).then(setStudents([...values.students]));
+  };
+
+  const updateStudent = (i) => {
+    return (student) => {
+      values.students[i] = student;
+      return updateFields(values).then(setStudents([...values.students]));
+    };
+  };
+
+  const removeStudent = (i) => {
+    return () => {
+      values.students.splice(i, 1);
+      return updateFields(values).then(setStudents([...values.students]));
+    };
+  };
+
   return (
     <Grid container spacing={SPACING}>
       <Grid item sm={12}>
@@ -380,10 +592,29 @@ export const ParentStudentDetails = ({ values }) => {
       </Grid>
       <Grid item md={12}>
         <List>
-          {values.students.map((student) => (
-            <StudentDetail student={student} />
+          {students.map((student, i) => (
+            <StudentDetail
+              student={student}
+              updateStudent={updateStudent(i)}
+              removeStudent={removeStudent(i)}
+            />
           ))}
+          {addingStudent && (
+            <StudentDetail
+              student={{ name: "", subjects: [], gradeLevel: "" }}
+              onComplete={() => setAddingStudent(false)}
+              isNew={true}
+              updateStudent={addStudent}
+            />
+          )}
         </List>
+      </Grid>
+      <Grid>
+        {!addingStudent && (
+          <Button variant="outlined" onClick={() => setAddingStudent(true)}>
+            Add Student
+          </Button>
+        )}
       </Grid>
     </Grid>
   );
