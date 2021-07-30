aa476a073ff185f3ed1702b649107ffb20769ad4
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2021-07-29 17:45:35 -0700

    @johanc/press release (#238)
    * raw html without css
    
    * some css
    
    * Added christin

---
+++ packages/client/src/pages/PressRelease/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PopUp from "./popup";
+
+const PressRelease = () => {
+    const content = (
+        <>
+            <i>Effective July 30, 2021</i>. CovEd will begin transitioning away from a user-directed mentor matching platform towards a partnerships-based model with local organizations, in which mentors will be directly hand-matched by CovEd with students in underserved K-12 communities. <b>Users will be unable to access their profile and the self-matching service starting August 20th.</b>
+            <br/>
+            <b>BOSTON, MA</b> ― As shifting outlooks regarding the COVID-19 pandemic continue to affect both K-12 students and mentors from higher education institutions across the U.S., CovEd will transition to a new model of 1-to-1 mentorship pairings that moves away from individualized, user-directed mentor pairing, toward a partnerships-based model that centers around local organizations dedicated to supporting underserved K-12 communities.
+            <br/>
+            Instead of pairing mentors and mentees directly through the CovEd website, <a href="www.coved.org">www.coved.org</a>, and thus placing the onus on families to search through the mentor database and initiate the mentor-mentee connection, CovEd will pair mentors with mentees from nonprofit, community, and school-based partnerships. 
+            <br/>
+            CovEd has recently piloted two versions of this model.  In this Spring 2021 pilot program, CovEd mentors were paired with and met with mentees 1-2 times per week during their student advisory periods, providing academic support in English and Math.
+            <br/>
+            This transition will not affect currently paired mentors and mentees. However, the website search portal will be closed and all future matches to mentors will be made internally. To ensure that CovEd is effectively maximizing the resources available to support as many students as possible,  there will be options for facilitated 1:1 mentor matching for prospective mentees who are not part of our partner organizations or school districts.
+            <br/>
+            <h3>FAQs</h3>
+            <h4>What will happen to my account information?</h4>
+            User emails will stay on our mailing list. You may choose to unsubscribe from it. All other account information will be deleted. Prior to deletion, CovEducation will collect aggregate statistical data to measure our past impact and to better serve our community in the future. 
+            <h4>What does this mean for prospective students looking for mentors?</h4> 
+            With the new transition, members of the CovEd organizational team will be the ones facilitating matches, rather than placing the onus on parents of K-12 students to search for and contact a mentor on their own. As such, there will no longer be a user-directed interface in which parents create an account and look through the mentor database for a potential match.
+            <br/>
+            While CovEd will be working primarily with non-profit organizations to ensure that they are reaching students from a wide range of backgrounds, they will continue to support students who are unaffiliated with partner organizations as much as possible. For select weeks in a given year (i.e. the start of the fall, spring, and summer semesters), they will run a pairing program for individuals unaffiliated with one of our existing partners. (More information on this to come at <a href="www.coved.org">www.coved.org</a>!)
+            <h4>What does this mean for current and prospective mentors?</h4> 
+            Current mentors who are listed as available will be ported over to our new system, and any individuals looking to join CovEd as a mentor will sign up as directed on the website. When assigned to a mentee, mentors should expect members of the CovEd organizational team to reach out by email, rather than families. This way, CovEd coordinators can ensure that mentors are able and willing to commit to a new mentee before connecting them with a family or organization serving K-12 students in need of academic support.
+            New mentors will be directed to register at <a href="www.coved.org">www.coved.org</a>, and CovEd will reach out by email once a match has been made. For anyone interested in getting involved in our organization as a group leader  (i.e. Outreach, Public Relations, Speaker Series, Partnerships, Mentorship Support, etc.), we have a wide variety of positions available! Please reach out to coved.management@gmail.com for more info.
+            <br/>
+            <h4>What does this mean for existing mentorship pairs?</h4> 
+            CovEd will continue to support existing pairs, from organizing mentor workshops, facilitating rematching for current mentees if needed, or holding regional networking events for mentor-mentee pairs. Mentors and mentees who were previously matched can continue to be paired together. CovEd will also be communicating to mentors and mentees about any additional steps if needed. 
+            <br/>
+            <h4>What will happen to all the non-mentorship resources (Speaker Series, Handbooks, ABC)?</h4> 
+            Handbooks and resources will still be available on our website! We will also continue to hold Speaker Series and programs like ABC Club, mentor-directed events, and 24/7 Piazza.
+            <br/>
+            <br/>
+            <i>CovEducation (or “CovEd”) is a community of undergraduates, postgraduates, students and educators from colleges and universities across the U.S. who provide mentorship and academic support to K-12 students in light of the pandemic. With an expanding community of 6,000+ volunteers from 467 universities, 5,500+ mentees across 50 states, and 200,000+ hits, CovEd provides resources to students faced with financial, familial, or other difficulties that impact their learning experience. For more information, visit <a href="www.coved.org">www.coved.org</a>.</i>
+            <br/>
+        </>
+    );
+    const prompt = "Starting on August 20th, 2021, CovEd will be transition to a partnership based model to better serve the needs of our communities. Self-pairing will no longer be available. Click to learn more."
+    return (<>
+    <div>
+        <PopUp content={content} title="CovEducation Transition to Partnerships for Mentor Matching" prompt={prompt}/> 
+    </div>
+    </>)
+}
+
+export default PressRelease;
